3add9f46cc163bfea45266011bfd6bf9cc57efe7
Thorley <thorley.eric@gmail.com>
2018-10-03 23:57:33 -0400

    note structure added. still need to do mvc and comment functionality

--- routes/routes.js
+++ routes/routes.js
@@ -1,40 +1,14 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-
-var mongoose = require("mongoose");
-
-var cheerio = require("cheerio");
-var request = require("request");
-var logger = require("morgan");
-var bodyParser = require("body-parser")
-
-var app = express();
-var PORT = process.env.PORT || 3000;
-app.use(express.static("public"));
-
-var db = require("./models")
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ringerDB";
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI , {useNewUrlParser: true});
-
+var express = require('express')
+var router = express.Router()
+var db = require("../models")
 //routes
-app.get("/", function(req, res){
+router.get("/", function(req, res){
     db.Article.find({}, function(err, data){
         res.render("index", {articles : data})    
     })
 });
 
-app.get("/scrape", function(req, res){
+router.get("/scrape", function(req, res){
     request("https://theringer.com/nba", function(error, response, html){
         if (!error && response.statusCode == 200) {
             var $ = cheerio.load(html);
@@ -49,8 +23,7 @@ app.get("/scrape", function(req, res){
                 result.img = $(this).parent("div").children("a").children("div").children("img").attr("src");
                  
                 db.Article.create(result)
-                .then(function(){
-                    location.reload();
+                .then(function(dbArticle){
                 })
                 .catch(function(err) {
                     return res.json(err);
@@ -121,6 +94,4 @@ app.delete("/articles/:id", function(req, res){
     });
 })
 
-app.listen(PORT, function(){
-    console.log("Server listening on: http://localhost: " + PORT)
-});
+module.exports = router
